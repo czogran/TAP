08bbb42b206f47e8be8919597fa292ca53aa09a6
Kacper Michalski <k.michalski.1@stud.elka.pw.edu.pl>
2021-04-04 02:32:33 +0200

    stala dodana

--- TASK1/transmitation.m
+++ TASK1/transmitation.m
@@ -11,10 +11,10 @@ Tp=1;
 h0 = 81;
 V0 = volume(h0);
 
-B = [1, 1, 1, 0, 0, 0; Th0/V0 - T0/V0, Tc0/V0 - T0/V0, Td0/V0 - T0/V0, Fh0/V0, Fc0/V0, Fd0/V0];
-A = [-a/(sqrt(sqrt((V0^3)*C0))), 0; Fh0*T0/(V0^2) + Fc0*T0/(V0^2) + Fd0*T0/(V0^2) - Fh0*Th0/(V0^2) - Fc0*Tc0/(V0^2) - Fd0*Td0/(V0^2), -(Fh0/V0 + Fc0/V0 + Fd0/V0)];
-C = [1, 0;0, 1];
-D = [0, 0, 0, 0, 0, 0; 0, 0, 0, 0 ,0 , 0];
+B = [1, 1, 1, 0, 0, 0; Th0/V0 - T0/V0, Tc0/V0 - T0/V0, Td0/V0 - T0/V0, Fh0/V0, Fc0/V0, Fd0/V0;0, 0, 0, 0, 0, 0];
+A = [-a*0.25/(sqrt(sqrt((V0^3)*C0))), 0, 1; Fh0*T0/(V0^2) + Fc0*T0/(V0^2) + Fd0*T0/(V0^2) - Fh0*Th0/(V0^2) - Fc0*Tc0/(V0^2) - Fd0*Td0/(V0^2), -(Fh0/V0 + Fc0/V0 + Fd0/V0), 0; 0, 0, 0];
+C = [1, 0, 0;0, 1, 0; 0, 0, 1];
+D = [0, 0, 0, 0, 0, 0; 0, 0, 0, 0 ,0 , 0; 0, 0, 0, 0, 0, 0];
 
 
 transmit = ss(A, B, C, D, 'InputDelay', [0, 180, 0, 0, 0, 0]);
@@ -26,7 +26,7 @@ t = 1:2000;
 u = [ones(1,2000)*Fh;ones(1,2000)*Fcin;ones(1,2000)*Fd;ones(1,2000)*Th;ones(1,2000)*Tc;ones(1,2000)*Td];
 
 figure
-lsim(transmit, u, t)
+lsim(transmit, u, t, [V0, T0, -a/sqrt(sqrt(C0))*(sqrt(sqrt(V0))-0.25*V0/sqrt(sqrt(V0^3)))])
 hold on
 %step(discreteTransmit)
 hold off
