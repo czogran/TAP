6004c27d600192bdb119bfb1487359fd5ada4f1f
czogran <pawel.rawicki@gmail.com>
2021-04-04 23:33:02 +0200

    tetsy

--- TASK1/test2.m
+++ TASK1/test2.m
@@ -1,135 +1,121 @@
-%%--SYMULACJA DZIAŁANIA OBIEKTU--%
-%Polecenie: Zasymulować działanie obiektu w Matlabie
+% STEP RESPONSES
 
-clc;
-clear;
-close all;
-
-%DATA
-%F-> dV/dT [cm^3/s]
-%T-> temperature
-
-%INPUTS
-%Hot water
-Th=62;
-Fh=14;
-%Cold water
-Tc=23;
-Fcin=37;
-%Discruption
-Td=33;
-Fd=12;
-
-%OUTPUT
-Tout=0;
-F=0;
-
-h=81;
-T=33.57;
-
-%ADJUSTABLE SIZES
-% h;
-% Tout;
-
-%CONTROL VALUES
-% Fh;
-% Fcin;
-
-%VARIABLES
-% time
-t=0:2000;
-
-%DELAY
-delay=120;
-delayC=180;
-
-%% filling tank
-h=zeros(length(t),1);
-V=[0]
-Tvector=[0];
-Fc=Fcin;
-for k=2:length(t)
-    if(k<delayC)
-        V(k)=V(k-1)+(Fh+Fd-outputFlow(h(k-1)));
-        dVdTdt=Fh*Th+Fd*Td-(Fh+Fd)*Tvector(k-1);
-    else
-        V(k)=V(k-1)+(Fh+Fc+Fd-outputFlow(h(k-1)));
-        dVdTdt=Fh*Th+Fc*Tc+Fd*Td-(Fh+Fc+Fd)*Tvector(k-1);
-    end
-    h(k)=heightFromVolume(V(k));
+%init variables
+init
     
-    Tvector(k)=Tvector(k-1)+ dVdTdt/V(k);
-end
+Tinputs=[Th,Tc,Td];
+F0inputs=[Fh,Fcin,Fd];
 
-figure
-plot(t,h)
-title("Napełnianie zbiornika")
-xlabel("t[s]");
-ylabel("h[cm]")
-hold off
+FcVector =[Fcin] ; 
+FhVector = Fh +[0,1]; 
 
-figure
-plot(t,Tvector)
-title("Temperatura w zbiorniku"+newline+"podczas napełniania zbiornika")
-xlabel("t[s]");
-ylabel("T[\circC]")
-hold off
+% FcVector=Fcin+10;
+FcVectorLength = length(FcVector);
+FhVectorLength = length(FhVector);
 
 
-%% static characteristics
-%!!!!! IN STATIC CHARACTERISTIC DELAY DOES NOT MATTER
+hVector=ones(length(t),FcVectorLength,FhVectorLength)*h0;
+hVectorLinearized=ones(length(t),FcVectorLength,FhVectorLength)*h0;
 
-h=81;
-V=[volume(h)]
-Tvector=[T];
-Fh=Fh;
+V=zeros(length(t),FcVectorLength,FhVectorLength);
+V0=volume(hVector(1));
+V(1,:,:)=V0;
+VL=zeros(length(t),FcVectorLength,FhVectorLength);
+VL(1,:,:)=volume(hVectorLinearized(1));
 
-FhStatic = (Fh-10) : (Fh+10);
-FcStatic = (Fc-10) : (Fc+10);
+Tvector=ones(length(t),FcVectorLength,FhVectorLength)*T0;
+TvectorL=ones(length(t),FcVectorLength,FhVectorLength)*T0;
 
-Vstatic = zeros(length(FhStatic), length(FcStatic));
-hStatic = zeros(length(FhStatic), length(FcStatic));
+TvectorOutput=ones(length(t),FcVectorLength,FhVectorLength)*T0;
+TvectorLOutput=ones(length(t),FcVectorLength,FhVectorLength)*T0;
 
-for i =  1: length(FhStatic)
-    Fh=FhStatic(i);
-    for  j=  1: length(FcStatic)
-        Fc=FcStatic(j);
-        h=81;
-        V=[volume(h)];
-        Tvector=[T];
-        for k=2:length(t)
-            V(k)=V(k-1)+(Fh+Fc+Fd-outputFlow(h));
-            h=heightFromVolume(V(k));
+for j=1:length(FhVector)
+    Fh=FhVector(j);
+    
+    for i=1:length(FcVector)    
+    
+    % rungy-kutta
+    for k=2:length(t)
+        delay=1;
 
-            dVdTdt=Fh*Th+Fc*Tc+Fd*Td-(Fh+Fc+Fd)*Tvector(k-1);
-            Tvector(k)=Tvector(k-1)+dVdTdt/V(k);
+        if(k<delayC)
+            Fc=Fcin;
+        else
+           Fc=FcVector(i);
+        end 
+        Finputs=[Fh,Fc,Fd];
+         
+        h=hVector(k-1,i,j);
+        hL=hVectorLinearized(k-1,i,j);
+        
+        T=Tvector(k-1,i,j);
+        TL=TvectorL(k-1,i,j);
+        
+        kV1= dVdt(h, delay, Finputs);
+        kV2= dVdt(heightFromVolume(V(k-1,i,j) + Tp/2*kV1),delay,Finputs);
+        kV3= dVdt(heightFromVolume(V(k-1,i,j) + Tp/2*kV2),delay,Finputs);
+        kV4= dVdt(heightFromVolume(V(k-1,i,j) + Tp*kV3),delay,Finputs);
+
+        dV=Tp/6*(kV1+2*kV2+2*kV3+kV4);
+        V(k,i,j)=V(k-1,i,j)+dV;
+        hVector(k,i,j)=heightFromVolume(V(k,i,j));
+
+        kV1L= dVdtLinearized(hL,h0, delay, Finputs);
+        kV2L= dVdtLinearized(heightFromVolume(VL(k-1,i,j) + Tp/2*kV1L),h0,delay,Finputs);
+        kV3L= dVdtLinearized(heightFromVolume(VL(k-1,i,j) + Tp/2*kV2L),h0,delay,Finputs);
+        kV4L= dVdtLinearized(heightFromVolume(VL(k-1,i,j) + Tp*kV3L),h0,delay,Finputs);
+
+        dVL=Tp/6*(kV1L+2*kV2L+2*kV3L+kV4L);
+        VL(k,i,j)=VL(k-1,i,j)+dVL;
+        hVectorLinearized(k,i,j)=heightFromVolume(VL(k,i,j));
+        
+                
+        kT1= dTdt(V(k-1,i,j),T,delay,Finputs,Tinputs);
+        kT2= dTdt(V(k-1,i,j) + Tp*V(k-1,i,j)/2,T + Tp/2*kT1,delay,Finputs,Tinputs);
+        kT3= dTdt(V(k-1,i,j) + Tp*V(k-1,i,j)/2,T + Tp/2*kT2,delay,Finputs,Tinputs);
+        kT4= dTdt(V(k-1,i,j) + Tp*V(k-1,i,j),T + Tp*kT3,delay,Finputs,Tinputs);       
+
+        dT=Tp/6*(kT1+2*kT2+2*kT3+kT4);
+        Tvector(k,i,j)=Tvector(k-1,i,j)+ dT;
+        
+
+        kT1L= dTdtLinearized(VL(k-1,i,j),V0,TL,T0,delay,Finputs,F0inputs,Tinputs);
+        kT2L= dTdtLinearized(VL(k-1,i,j) + Tp/2*kV1L,V0,TL + Tp/2*kT1L,T0,delay,Finputs,F0inputs,Tinputs);
+        kT3L= dTdtLinearized(VL(k-1,i,j) + Tp/2*kV2L,V0,TL + Tp/2*kT2L,T0,delay,Finputs,F0inputs,Tinputs);
+        kT4L= dTdtLinearized(VL(k-1,i,j) + Tp*kV3L,V0,TL + Tp*kT3L,T0,delay,Finputs,F0inputs,Tinputs);
+        
+        dTLinearized=Tp/6*(kT1L+2*kT2L+2*kT3L+kT4L);
+        TvectorL(k,i,j)=TvectorL(k-1,i,j)+dTLinearized;
+        
+        if(k<=delayT)
+            TvectorOutput(k,i,j)=T0;
+            TvectorLOutput(k,i,j)=T0;
+        else
+            TvectorOutput(k,i,j)=Tvector(k-delayT,i,j);
+            TvectorLOutput(k,i,j)=TvectorL(k-delayT,i,j);
         end
-        Vstatic(i,j) = V(end);
-        hStatic(i,j)=heightFromVolume(V(end));
     end
 end
-
-figure
-surf(FcStatic, FhStatic,  hStatic)
-xlabel('Fh');
-ylabel('Fc');
-title("Charakterystyka statyczna"+ newline+"wysokości słupa cieczy w zależności od"+newline+"dopływu cieplej i zimnej wody");
-hold off
-
-function h= heightFromVolume(V)
-    %C-> constant
-    C=0.7;    
-    h=sqrt(V/C);
 end
 
-function Tout=outputTemperature(t)
-    %delay-> constant;
-    delay=120;
-    Tout;
-end
 
-function Fc=flowC(t)
-    %delayC-> constant;
-    delayC=180;
-    Fc;
+
+
+legendLabels=[""];
+for j=1:FcVectorLength
+    figure
+    for i=1:FhVectorLength
+        plot(t,TvectorOutput(:,j,i), 'r')
+        hold on
+        plot(t,TvectorLOutput(:,j,i),'g')
+        hold on
+        legendLabels(2*i-1)="Fc[$\frac{cm^3}{s}$]:" +FhVector(i);
+        legendLabels(2*i)="linearyzacja Fc[$\frac{cm^3}{s}$]:" +FhVector(i);
+    end
+    title("Odpowiedzi skokowe temperatury wyjsciowej dla"+newline+"Fc[$\frac{cm^3}{s}$]:" +FcVector(j),'Interpreter', 'latex');
+    legend(legendLabels, 'Location', 'best', 'Interpreter','latex')
+    xlabel("t[s]");
+    ylabel("T[\circC]")
+    hold off
+    
 end
--- TASK1/transmitation.m
+++ TASK1/transmitation.m
@@ -14,19 +14,20 @@ transmit2 = tf(1, [1 ,a* 0.25/sqrt(sqrt(C*(V0^3)))],'InputDelay',180);
 transmit3 = tf(1, [2*C*h0,a* 0.5/sqrt(h0) ],'InputDelay',180);
 
 
- Numerator = {[Fh*Th-Fh*T0] [0.8 72.3]}; %Numerators of u_1 and u_2
- Denominator = {[V0, Fh+Fd+Fcin] [1 21.8 60]}; %Denominators of u_1 and u_2
+ Numerator = {[Th-T0] [0.8 72.3]}; %Numerators of u_1 and u_2
+ Denominator = {[V0, (Fh+Fd+Fcin)] [1 21.8 60]}; %Denominators of u_1 and u_2
  transformT = tf(Numerator,Denominator)
 
-% 0.25/sqrt(sqrt(C*(V0^3)
+figure
+step(transformT)
 
 
 method='zoh';
 discreteTransmit=c2d(transmit,Tp,method); % transmitancja dyskretna
 
-figure
-step(transmit)
-hold on
-step(discreteTransmit)
-hold off
+% figure
+% step(transmit)
+% hold on
+% step(discreteTransmit)
+% hold off
 
