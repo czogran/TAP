d7ad2040f356f7673d2d166e444bbdf0075899ff
czogran <pawel.rawicki@gmail.com>
2021-04-06 17:19:04 +0200

    Wykresy transmitancji

--- TASK1/transmitation.m
+++ TASK1/transmitation.m
@@ -9,57 +9,38 @@ Tp=2;
 
 V0=volume(81);
 
-transmitVFc = tf(1, [1 ,a* 0.25/sqrt(sqrt(C*(V0^3)))],'InputDelay',180);
-transmitHFc = tf(1, [2*C*h0,a* 0.5/sqrt(h0) ],'InputDelay',180);
+transformVFc = tf(1, [1 ,a* 0.25/sqrt(sqrt(C*(V0^3)))],'InputDelay',180);
+transformHFc = tf(1, [2*C*h0,a* 0.5/sqrt(h0) ],'InputDelay',delayC);
 
-transmitVFh = tf(1, [1 ,a* 0.25/sqrt(sqrt(C*(V0^3)))]);
-transmitHFh = tf(1, [2*C*h0,a* 0.5/sqrt(h0) ]);
+transformVFh = tf(1, [1 ,a* 0.25/sqrt(sqrt(C*(V0^3)))]);
+transformHFh = tf(1, [2*C*h0,a* 0.5/sqrt(h0) ]);
 
-transmitVFd = tf(1, [1 ,a* 0.25/sqrt(sqrt(C*(V0^3)))]);
-transmitHFd = tf(1, [2*C*h0,a* 0.5/sqrt(h0) ]);
-
-transmitVFcFigure=figure
-step(transmitVFc)
-title("Transmitancja objętości w zbiorniku po skoku Fc")
-
-transmitVFhFigure=figure
-step(transmitVFh)
-title("Transmitancja objętości w zbiorniku po skoku Fh")
-
-transmitVFdFigure=figure
-step(transmitVFd)
-title("Transmitancja objętości w zbiorniku po skoku Fd")
+transformVFd = tf(1, [1 ,a* 0.25/sqrt(sqrt(C*(V0^3)))]);
+transformHFd = tf(1, [2*C*h0,a* 0.5/sqrt(h0) ]);
 
 
 
 
+% TEMPERATURE
+transformTFh=tf([Th-T0],[V0, (Fh+Fd+Fcin)], 'OutputDelay', delayT );
+transformTFc=tf([Tc-T0],[V0, (Fh+Fd+Fcin)],'InputDelay',delayC, 'OutputDelay', delayT);
+transformTFd=tf([Td-T0],[V0, (Fh+Fd+Fcin)], 'OutputDelay', delayT );
 
+transformTV=tf ([((Fh+Fcin+Fd)*T0-(Fh*Th+Fcin*Tc+Fd*Td))/V0],[V0, (Fh+Fd+Fcin)], 'OutputDelay', delayT );
 
+transformTTh=tf(Fh,[V0, (Fh+Fd+Fcin)], 'OutputDelay', delayT );
+transformTTc=tf(Fcin,[V0, (Fh+Fd+Fcin)], 'InputDelay',delayC, 'OutputDelay', delayT );
+transformTTd=tf(Fd,[V0, (Fh+Fd+Fcin)], 'OutputDelay', delayT );
 
-transmitHFcFigure=figure
-step(transmitHFc)
-title("Transmitancja wysokości słupa cieczy w zbiorniku po skoku Fc")
+plotTransforms
 
-transmitHFhFigure=figure
-step(transmitHFh)
-title("Transmitancja wysokości słupa cieczy w zbiorniku po skoku Fh")
-
-transmitHFhFigure=figure
-step(transmitHFd)
-title("Transmitancja wysokości słupa cieczy w zbiorniku po skoku Fd")
-
-
+%  Numerator = {[Th-T0] [0.8 72.3]}; %Numerators of u_1 and u_2
+%  Denominator = {[V0, (Fh+Fd+Fcin)] [1 21.8 60]}; %Denominators of u_1 and u_2
+%  transformT = tf(Numerator,Denominator)
+% 
 % figure
-% step(transmitH)
-
-
- Numerator = {[Th-T0] [0.8 72.3]}; %Numerators of u_1 and u_2
- Denominator = {[V0, (Fh+Fd+Fcin)] [1 21.8 60]}; %Denominators of u_1 and u_2
- transformT = tf(Numerator,Denominator)
-
-figure
-stepplot(transformT)
-title("ww")
+% stepplot(transformT)
+% title("ww")
 
 % method='zoh';
 % discreteTransmit=c2d(transmit,Tp,method); % transmitancja dyskretna
